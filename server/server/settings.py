367d517e91c3b1a86a2043844f7973d6bc5a73d5
SultokTheF <sultok.003@gmail.com>
2024-04-14 13:36:12 +0600

    db

--- server/server/settings.py
+++ server/server/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'db-name',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
